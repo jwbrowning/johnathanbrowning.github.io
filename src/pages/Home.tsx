62c3e28bd4ef55a333de86632f271cd2f520b44c
Johnathan Browning <jwbrowning19@gmail.com>
2026-02-19 13:03:07 -0800

    update description

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,9 @@ import { professionalProjects, personalProjects } from "../data/projects"
 import ProjectCard from "../components/ProjectCard"
 import Layout from "../components/Layout"
 
-const description = "Software Engineer specializing in Unity development with 2+ years of professional experience architecting real-time 3D simulation systems in C#. Focused on deterministic system design, modular gameplay logic, and translating complex requirements into maintainable interactive applications. Below are selected projects highlighting both professional and personal work. Reach me at contact@johnathanbrowning.com";
+const description = "Unity Software Engineer with 2+ years of experience building real-time 3D simulations in C#. My work centers on modular gameplay systems, deterministic logic, and translating complex technical specifications into interactive software.";
+const description2 = "Below are my professional experiences and selected personal projects."
+const description3 = "Reach me at contact@johnathanbrowning.com"
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
         <header className="site-header">
             <h1 className="centered-text">Johnathan Browning</h1>
             <p className="centered-text">{description}</p>
+            <p className="centered-text">{description2}</p>
+            <p className="centered-text">{description3}</p>
         </header>
 
         <div className="home-grid">
